6990ba0569666b65f96bd5f233c40ef7d2b1e703
Marius <Mariusbe15@gmail.com>
2025-01-04 13:50:42 +0100

    Contribution day: 04/01/2025

--- gitauto_pull.py
+++ gitauto_pull.py
@@ -11,17 +11,20 @@ def write_number(number):
         file.write(str(number)) 
 
 
-def git_commit():
+def git_push_changes():
+    try:
 
-    # Run git add . in "CLI"
-    obj = subprocess.run(['git','add','.'])
-    # current date
-    date = datetime.datetime.now().strftime('%d/%m/%Y')
-    message = f'Contribution day: {date}'
-    # Commit the changes
-    subprocess.run(['git','commit','-m',message])
-    # Push changes to repository
-    subprocess.run(['git','push'])
+        # Run git add . in "CLI"
+        obj = subprocess.run(['git','add','.'])
+        # current date
+        date = datetime.datetime.now().strftime('%d/%m/%Y')
+        message = f'Contribution day: {date}'
+        # Commit the changes
+        subprocess.run(['git','commit','-m',message])
+        # Push changes to repository
+        subprocess.run(['git','push'])
+    except subprocess.CalledProcessError as e:
+        print(f"Error {str(e)}")
     
 
 def main():
@@ -29,7 +32,7 @@ def main():
         current_number = read_number() 
         number_to_change = current_number + 1 
         add_number = write_number(number_to_change)
-        git_commit()
+        git_push_changes()
     except Exception as e:
         print(f"Error: {str(e)}")
         exit(1)
