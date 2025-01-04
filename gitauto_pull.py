b749841a37073b6d8bc32dbf01de4a6e9dc44bdc
Marius <Mariusbe15@gmail.com>
2025-01-04 13:07:48 +0100

    random

---
+++ gitauto_pull.py
@@ -0,0 +1,23 @@
+import os
+
+def read_number():
+    with open("text.txt","r") as file:
+        return int(file.read()) # --> retorna un int
+
+def write_number(number): #recibe el nuevo numero
+    with open("text.txt","w") as file:
+        file.write(str(number)) # --> pasa el nuevo numero a string
+
+current_number = read_number() # --> retorna un unico numero y lo almacena
+number_to_change = current_number + 1 # --> suma + 1 a ese numero
+add_number = write_number(number_to_change) # --> añade el numero del tipo int, y lo escribe en string
+
+# TODO : ACTUALIZAR LOS CAMBIOS DE LA CARPETA, GIT ADD, GIT COMMIT -M "NAME", GIT PUSH  TO REPOSITORY
+
+def git_commit():
+
+
+# for i in range(5):
+#     current_number = read_number()
+#     next_number = current_number + 1
+#     write_number(next)
